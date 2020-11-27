f8a540adefc9938cb805b69ca10e2d481f9220a6
Diwa Wardak <diwawardak1@gmail.com>
2020-11-27 17:40:02 +0000

    home page added to nav

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
         <Header className="header-color" title="MY PORTFOLIO" scroll>
             <Navigation>
                 {/* <Link to="/aboutme">About Me</Link> */}
+                <Link to="/">Home</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
